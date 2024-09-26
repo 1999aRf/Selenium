a9ff216066caec3e9668d583ad43593742fb6180
rinatagliulov <rinat.agliulov@rentready.com>
2024-09-26 12:57:12 -0400

    Написал простой тест для домашней работы.

---
+++ selenium-java/src/test/java/firstTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Objects;
+
+public class firstTest {
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    @BeforeAll
+    public static void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("https://www.deepl.com/ru/translator");
+    }
+
+    @AfterAll
+    public static void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
