cef50c9ce2fa9d5f6f7bcbb93ba1a957195e7536
rinatagliulov <rinat.agliulov@rentready.com>
2024-10-11 10:00:03 -0400

    Выполнил 7 задание.

--- selenium-java/src/test/java/secondTest.java
+++ selenium-java/src/test/java/secondTest.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.time.Duration;
+import java.util.stream.Collectors;
 
 public class secondTest {
     private static WebDriver driver;
@@ -176,6 +179,46 @@ public class secondTest {
 
         driver.findElement(By.linkText("vQmods")).click();
         driver.findElement(By.tagName("h1"));
+    }
+
+    @Test
+    public void sevenTask() {
+        driver.get("http://localhost/litecart");
+        // WebElement element;
+        List<WebElement> yellowDuck = driver.findElements(By.cssSelector("#box-most-popular a[title='Yellow Duck'] div.sticker.sale"));
+        if (yellowDuck.size() == 1) {
+            System.out.println("Элемент " + yellowDuck + "найден и он единственный.");
+        } else {
+            System.out.println("Найдено элементов: " + yellowDuck.size());
+        }
+
+        List<WebElement> greenDucks = driver.findElements(By.cssSelector("#box-most-popular a[title='Green Duck'] div.sticker.new"));
+        if (greenDucks.size() == 1) {
+            System.out.println("Элемент " + greenDucks + "найден и он единственный.");
+        } else {
+            System.out.println("Найдено элементов: " + greenDucks.size());
+        }
+
+        List<WebElement> blueDucks = driver.findElements(By.cssSelector("#box-most-popular a[title='Blue Duck'] div.sticker.new"));
+        if (blueDucks.size() == 1) {
+            System.out.println("Элемент " + blueDucks + "найден и он единственный.");
+        } else {
+            System.out.println("Найдено элементов: " + blueDucks.size());
+        }
+
+        List<WebElement> redDucks = driver.findElements(By.cssSelector("#box-most-popular a[title='Red Duck'] div.sticker.new"));
+        if (redDucks.size() == 1) {
+            System.out.println("Элемент найден и он единственный.");
+        } else {
+            System.out.println("Найдено элементов: " + redDucks.size());
+        }
+
+        List<WebElement> purpleDucks = driver.findElements(By.cssSelector("#box-most-popular a[title='Purple Duck'] div.sticker.new"));
+        if (purpleDucks.size() == 1) {
+            System.out.println("Элемент найден и он единственный.");
+        } else {
+            System.out.println("Найдено элементов: " + purpleDucks.size());
+        }
     }
 
     @AfterAll
